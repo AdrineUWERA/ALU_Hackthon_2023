abf536c5e90701de0ba5838fb3e5089b9493e13a
gabin-ishimwe <g.ishimwe@alustudent.com>
2023-04-22 15:15:09 +0200

    ft: seed data

---
+++ prisma/data.js
@@ -0,0 +1,86 @@
+ const courses = [
+    {
+        name: "AI",
+        topics: [
+            {
+                name: "Introduction to AI",
+                description: "...",
+                subTopics: [
+                    {
+                        title: "Definition of AI",
+                        content: "..."
+                    },
+                    {
+                        title: "Brief history of AI",
+                        content: "..."
+                    },
+                    {
+                        title: "Applications of AI",
+                        content: "..."
+                    },
+                ]
+            },
+            {
+                name: "Machine Learning",
+                description: "...",
+                subTopics: [
+                    {
+                        title: "Introduction to machine learning",
+                        content: "..."
+                    },
+                    {
+                        title: "Supervised learning",
+                        content: "..."
+                    },
+                    {
+                        title: "Unsupervised learning",
+                        content: "..."
+                    },
+                ]
+            },
+        ]
+    },
+    {
+        name: "Networking",
+        topics: [
+            {
+                name: "Introduction to Networking",
+                description: "...",
+                subTopics: [
+                    {
+                        title: "What is networking?",
+                        content: "..."
+                    },
+                    {
+                        title: "Types of networks",
+                        content: "..."
+                    },
+                    {
+                        title: "Network topologies",
+                        content: "..."
+                    },
+                ]
+            },
+            {
+                name: "Physical Layer",
+                description: "...",
+                subTopics: [
+                    {
+                        title: "Introduction to the physical layer",
+                        content: "..."
+                    },
+                    {
+                        title: "Networking cables and connectors",
+                        content: "..."
+                    },
+                    {
+                        title: "Network topologies and media",
+                        content: "..."
+                    },
+                ]
+            },
+        ]
+    },
+]
+
+module.exports = {courses}
