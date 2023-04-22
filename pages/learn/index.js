4a9da6638f0ed33fb672af49e5c358506f9b5191
Evelyne <e.umubyeyi@alustudent.com>
2023-04-22 19:29:35 +0200

    Added course menu page

--- pages/learn/index.js
+++ pages/learn/index.js
@@ -1,5 +1,12 @@
-const CourseMenu = () => {
-    return ( <div>  courses</div> );
+import React from "react";
+import CourseMenu from "./../../src/components/CourseMenu";
+import Footer from "./../../src/components/Footer";
+
+export default function CourseList() {
+    return (
+    <React.Fragment>
+        <CourseMenu/>
+        <Footer/>
+      </React.Fragment>
+    );
 }
- 
-export default CourseMenu;
