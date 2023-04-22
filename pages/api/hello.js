14dccf83dab4695477396122af43b0248d11d462
gabin-ishimwe <g.ishimwe@alustudent.com>
2023-04-22 11:38:43 +0200

    ch: setup mongodb

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  console.log(req)
+  if(req.method == 'GET') {
+    const data = await prisma.questions.create({
+     data: {
+      name: "testing"
+     }
+    })
+    res.status(200).json(data)
+  } else {
+    res.status(500).send("Undefined Request Method")
+  }
 }
