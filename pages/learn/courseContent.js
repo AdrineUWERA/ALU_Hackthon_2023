b1f520d65e368f1aa67d37e2ceac96ed3f181734
Evelyne <e.umubyeyi@alustudent.com>
2023-04-23 06:07:24 +0200

    Worked on adjusting course content

--- pages/learn/courseContent.js
+++ pages/learn/courseContent.js
@@ -17,7 +17,48 @@ const courseContent = () => {
         const result = await fetch('http://localhost:3000/api/content/64447b33f68eb82f01b7a3f6');
         const json = await result.json();
         setCourse(json);
-    };   
+    };
+    
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+    
+    const regenerateContent = async (level) => {
+        const result = await fetch('http://localhost:3000/api/regenerate', {
+            method: 'POST',
+            body: JSON.stringify({"content": content, "level":level }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const json = await result.json();
+        if(json.text !== ""){
+            setContent(json.text);
+        }
+        toggleMenu()
+    };  
+
+    const summarizeContent = async (level) => {
+        const result = await fetch('http://localhost:3000/api/summarize', {
+            method: 'POST',
+            body: JSON.stringify({"prompt": content}),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const json = await result.json();
+        if(json.text !== ""){
+            setContent(json.text);
+        }
+        toggleMenu()
+    }; 
+
+    const regenerateFunction = async (level) => {
+        if(level === 'summarize'){
+
+        }else{
+            regenerateContent(level)
+        }
+
+    }
     
     const [isHidden, setIsHidden] = useState(true);
 
@@ -42,7 +83,6 @@ const [selectedTopic, setSelectedTopic] = useState(0);
 //   let content_title = '';
   function getContent(){
     for(let i = 0; i< course.topics.length; i++){
-        console.log(i)
         if(course.topics[i].id === selectedTopic){
             let courseSubTopics = course.topics[i].subTopics
             for(let j = 0; j< courseSubTopics.length; j++){
@@ -94,8 +134,31 @@ const [selectedTopic, setSelectedTopic] = useState(0);
                           </div>
                           </aside>
                           <div id="main_content" className="p-4 sm:ml-64">
-                              <div class="p-4 border-2 border-indigo-600 rounded-lg">
+                              <div class="p-4 border-2 border-grey-600 rounded-lg">
                                   <div class="container mx-auto px-4">
+                                  <div class="flex justify-end">
+                                    <button class="inline-flex gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 mr-4">Practice Quiz</button>
+                                    <div class="relative inline-block text-left">
+  <div>
+    <button type="button" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true" onClick={toggleMenu}>
+      Adjust Content
+      <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+      </svg>
+    </button>
+  </div>
+  {isMenuOpen && (
+  <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+    <div class="py-1" role="none">
+      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0" onClick={() => regenerateFunction('beginner')}>Beginner friendly</a>
+      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1" onClick={() => regenerateFunction('intermediate')}>Intermediate level</a>
+      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2" onClick={() => regenerateFunction('advanced')}>Advanced level</a>
+      <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2" onClick={() => regenerateFunction('summarize')}>Summarize Content</a>
+    </div>
+  </div>
+  )}
+</div>
+                                </div>
                                       <h1 class="text-4xl font-bold text-gray-900 mt-8 mb-4">{contentTitle}</h1>
                                       <div class="py-4">
                                           {content}
